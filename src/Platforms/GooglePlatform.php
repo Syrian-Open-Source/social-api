8c99405b9f533591324981bda1ca06cf0e54ae68
Marikamal <mary30ali@gmail.com>
2024-01-29 14:00:28 +0300

    updates in GooglePlatform file

--- src/Platforms/GooglePlatform.php
+++ src/Platforms/GooglePlatform.php
@@ -2,7 +2,47 @@
 
 namespace SOS\SocialApi\Platforms;
 
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 class Google extends AbstractPlatform
 {
     // Google specific logic and data
+
+    protected $scopes = [
+        'openid', 
+        'user_profile', 
+        'user_email', 
+    ];
+    protected $baseUrl = 'https://www.googleapis.com/oauth2/v3/userinfo';
+
+    public function getUserInfo()
+    {
+        try{
+            $response = $this->sendRequest($this->baseUrl);
+            $response->throw();
+            return $response->json();   
+       }
+       catch(\Exception $e){  
+            Log::error('Failed to retrieve user information from Google API: ' . $e->getMessage());
+       }
+    }
+
+    public function mapUserData($data)
+    {
+        // Mapping user information      
+        try{
+            return [
+                'id' => $data['sub'],
+                'nickname' => $data['given_name'], 
+                'name' => $data['name'],
+                'email' => $data['email'],
+            ];
+        }
+        catch(\Exception $e){          
+            Log::error('An exception occurred while mapping user information:  ' . $e->getMessage());   
+        }
+    }
 }
