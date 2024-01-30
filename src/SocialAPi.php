e2f38a88ab5af66586b180f90b88eeecdbb450fe
Somar <kesen.somar.99@gmail.com>
2024-01-30 14:42:40 +0300

    Ref (Main Class): Platform Handling & Dynamic Platform Initialization & Improved Validation

--- src/SocialAPi.php
+++ src/SocialAPi.php
@@ -2,7 +2,11 @@
 
 namespace SOS\SocialApi;
 
-use Illuminate\Support\Facades\Validator;
+use SOS\SocialApi\Platforms\LinkedIn;
+use SOS\SocialApi\Platforms\Google;
+use SOS\SocialApi\Platforms\Facebook;
+use SOS\SocialApi\Platforms\Github;
+use InvalidArgumentException;
 
 /**
  * @author by: Somar Kesen.
@@ -11,15 +15,22 @@ use Illuminate\Support\Facades\Validator;
  */
 class SocialApi
 {
-    public function Login($platform)
-    {
-        $validator = Validator::make(['platform' => $platform], [
-            'platform' => 'required|in:Google,Facebook,Github,LinkedIn',
-        ]);
+    protected $platforms = [
+        'Google' => Google::class,
+        'Facebook' => Facebook::class,
+        'Github' => Github::class,
+        'LinkedIn' => LinkedIn::class,
+    ];
 
-        if ($validator->fails()) {
-            // Handle the error scenario
-            throw new \InvalidArgumentException('Validation failed: ' . $validator->errors());
+    public function login($platformName, $token)
+    {
+        if (!array_key_exists($platformName, $this->platforms)) {
+            throw new InvalidArgumentException("Unsupported platform: $platformName");
         }
+
+        $platformClass = $this->platforms[$platformName];
+        $platformInstance = new $platformClass($token);
+
+        return $platformInstance->getUserInfo();
     }
 }
